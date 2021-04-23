d69964e2e2235a4f6f6f19b8fc1ac4ce7481e63a
matteo-andreuzza <76852004+matteo-andreuzza@users.noreply.github.com>
2021-04-23 23:25:02 +0200

    download solo URL

---
+++ main.py
@@ -0,0 +1,25 @@
+#import:
+import os
+from io import BytesIO
+import time
+import subprocess
+#print di versione:
+print("easy youtube-dl versione 0.0.1")
+print("pronto")
+#scrittura URL
+print("")
+print("")
+URL = input("inserisci qui l'url del video di youtube:...")
+print("")
+name = input("ok, ora inserisci il nome del file in cui vuoi salvare il video:...")
+print("")
+estensione = input("inserisci ora il tipo di file in cui voui salvare il video (per l'elenco delle estensioni supportate digita 'types'):...")
+if estensione == 'types':
+    print(".mp4")
+    print(".avi")
+print("")
+print("inizializzazione procedura di conversione...")
+#comandi:
+os.chdir("C:/Users/matte/Desktop")
+alfa = subprocess.Popen("youtube-dl " + URL, shell=True, stdout=subprocess.PIPE ).stdout.read()
+print("fatto")
